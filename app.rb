f106adeead43ea0698ad2abe420d9009e95540a9
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 18:18:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,5 +11,5 @@ post '/' do
   #the first part we can make up = the second part is already assigned in names in index
   the_animal_choice = params[:user_animal]
   the_hat_choice = params[:user_hat]
-  
+  @animal_pic = get_animal(the_animal_choice, the_hat_choice)
 end
