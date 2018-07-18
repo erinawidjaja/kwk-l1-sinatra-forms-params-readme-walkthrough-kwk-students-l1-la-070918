a3341242ee7c7b3f2dcc2d1f88634ad7d0458ec9
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 18:19:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,4 +12,7 @@ post '/' do
   the_animal_choice = params[:user_animal]
   the_hat_choice = params[:user_hat]
   @animal_pic = get_animal(the_animal_choice, the_hat_choice)
+  erb:results
+end
+
 end
