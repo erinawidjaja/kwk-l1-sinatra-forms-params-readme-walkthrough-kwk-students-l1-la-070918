7e287481e9912b2be1a4d0e1da3d39f89c083a9b
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 17:39:41 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -3,10 +3,15 @@ def get_animal(animal,hat)
   if animal == "dog"
       if hat == "party-hat"
         animal_image = "https://i.pinimg.com/736x/5c/29/3c/5c293cf4c57c45fe9477d73887a11eb6--birthday-party-hats-birthday-wishes.jpg"
+      elsif hat == "top-hat"
+        animal image = "http://www.furrypartners.com/images/P/Tux_with_Tails_Top_Hat_ZW529.jpg"
+      elsif hat == "bucket-hat"
+        animal image = "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
+      end 
+  elsif animal == "cat"
+  elsif hat == "party-hat"
+        animal_image = "https://i.pinimg.com/736x/5c/29/3c/5c293cf4c57c45fe9477d73887a11eb6--birthday-party-hats-birthday-wishes.jpg"
       if hat == "top-hat"
         animal image = "http://www.furrypartners.com/images/P/Tux_with_Tails_Top_Hat_ZW529.jpg"
       if hat == "bucket-hat"
         animal image = "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
-        
-  elsif animal == "cat"
-  
