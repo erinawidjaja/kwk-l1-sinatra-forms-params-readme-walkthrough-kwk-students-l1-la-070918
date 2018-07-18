eeec476a0462dbee7c4d681ef0c892249cb7eb3a
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 17:33:42 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -4,7 +4,7 @@ def get_animal(animal,hat)
       if hat == "party-hat"
         animal_image = "https://i.pinimg.com/736x/5c/29/3c/5c293cf4c57c45fe9477d73887a11eb6--birthday-party-hats-birthday-wishes.jpg"
       if hat == "top-hat"
-        animal image = ""
+        animal image = "http://www.furrypartners.com/images/P/Tux_with_Tails_Top_Hat_ZW529.jpg"
       if hat == "bucket-hat"
         animal image = ""
         
