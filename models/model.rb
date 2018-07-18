2fe4504c0473e8077bc34a0389edd6abac13a517
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 17:43:44 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -4,17 +4,17 @@ def get_animal(animal,hat)
       if hat == "party-hat"
         animal_image = "https://i.pinimg.com/736x/5c/29/3c/5c293cf4c57c45fe9477d73887a11eb6--birthday-party-hats-birthday-wishes.jpg"
       elsif hat == "top-hat"
-        animal image = "http://www.furrypartners.com/images/P/Tux_with_Tails_Top_Hat_ZW529.jpg"
+        animal_image = "http://www.furrypartners.com/images/P/Tux_with_Tails_Top_Hat_ZW529.jpg"
       elsif hat == "bucket-hat"
-        animal image = "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
+        animal_image = "https://i.pinimg.com/originals/b7/e2/40/b7e24003f4140a13495ff7b51558b5fe.jpg"
       end 
   elsif animal == "cat"
   if hat == "party-hat"
         animal_image = "https://www.thedailymeal.com/sites/default/files/story/2017/Cat%20hat%20hero_edit_0.jpg"
       elsif hat == "top-hat"
-        animal image = "http://cutecatsinhats.com/wp-content/uploads/2016/01/monocle-top-hat-cat.jpg"
+        animal_image = "http://cutecatsinhats.com/wp-content/uploads/2016/01/monocle-top-hat-cat.jpg"
       elsif hat == "bucket-hat"
-        animal image = "http://3.bp.blogspot.com/_HhUjJ73jZso/SBeeXiJPbaI/AAAAAAAAFCk/Jl4suaJdUkg/s400/bucket+hat5.jpg"
+        animal_image = "http://3.bp.blogspot.com/_HhUjJ73jZso/SBeeXiJPbaI/AAAAAAAAFCk/Jl4suaJdUkg/s400/bucket+hat5.jpg"
       end 
     end 
     return animal_img 
