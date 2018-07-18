75848549a769b2c90919b2fc3a997dd160097c90
erinawidjaja <erina.widjaja@gmail.com>
2018-07-18 17:30:54 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,12 @@
 #THIS IS WHERE OUR RUBY GOES 
 def get_animal(animal,hat)
-  
+  if animal == "dog"
+      if hat == "party-hat"
+        animal_image = ""
+      if hat == "top-hat"
+        animal image = ""
+      if hat == "bucket-hat"
+        animal image = ""
+        
+  elsif animal == "cat"
   
